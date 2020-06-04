487bf242844cff3b6d77c83c22a26a0951e13d89
Baidu <baidu@linliyu-shdeiMac.local>
2020-06-04 17:43:57 +0800

    修改屏幕

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo : 'git@github.com:401461843/express.git',
       path : '/home/express',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'cnpm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
