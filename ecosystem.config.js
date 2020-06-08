5af8ff39ad8c5ace2b934511b215986e907608aa
Baidu <baidu@linliyu-shdeiMac.local>
2020-06-08 13:52:08 +0800

    配置开机自动重启测试

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  apps : [{
-    script: './bin/www', //改成 ‘./bin/wwww’ 之后才暴露出3000端口，才难被访问
-    watch: '.',
-    name:'express',
-    instances: "max",
-    exec_mode: "cluster",
-    env: {
-      NODE_ENV: 'development'
+  'apps' : [{
+    'script': './bin/www', //改成 ‘./bin/wwww’ 之后才暴露出3000端口，才难被访问
+    'watch': '.',
+    'name':'express',
+    'instances': "max",
+    'exec_mode': "cluster",
+    'autorestart':true,
+    'env': {
+      'NODE_ENV': 'development'
     },
-    env_production : {
-      NODE_ENV: 'production'
+    'env_production' : {
+      'NODE_ENV': 'production'
     },
     "error_file": "./logs/pm2_express_err.log",
     "out_file": "./logs/pm2_express_out.log",
@@ -19,13 +20,14 @@ module.exports = {
     "log_date_format": "YYYY-MM-DD HH:mm:ss Z"
   }],
 
-  deploy : {
-    production : {
-      user : 'root',
-      host : ['116.62.121.90','106.12.69.208'],//这里可以多个远程服务器地址
-      ref  : 'origin/master', 
-      repo : 'git@github.com:401461843/express.git',
-      path : '/home/express',
+  'deploy' : {
+    'production' : {
+      'user' : 'root',
+      'host' : ['116.62.121.90','106.12.69.208'],//这里可以多个远程服务器地址
+      'ref'  : 'origin/master', 
+      'repo' : 'git@github.com:401461843/express.git',
+      'path' : '/home/express',
+      "ssh_options": "StrictHostKeyChecking=no",
       'pre-deploy-local': '',
       "pre-deploy": "git pull",//解决更新无法拉取github最新的commit
       'post-deploy' : 'cnpm install  && pm2 reload ecosystem.config.js --env production',
