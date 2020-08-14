de11bd0f96ed8ebc52ccdcfcc22ea033a13ee30f
Baidu <baidu@linliyu-shdeiMac.local>
2020-08-14 20:55:03 +0800

    初步完成

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//七夕
 router.get('/', function(req, res) {
 	res.render('index.html');
 });
+router.get('/lfx', function(req, res) {
+	res.render('lfx.html');
+});
+router.get('/game', function(req, res) {
+	res.render('game.html');
+});
+
+
+//苏州
 router.get('/zbh', function(req, res) {
 	res.render('zbh.html');
 });
